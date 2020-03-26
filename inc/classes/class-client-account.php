04bd0f2c9b63eac4edb92fc5ae3618a9fdcd52c1
thrijith <thrijith13@gmail.com>
2020-03-26 19:13:58 +0530

    Update account activation flow for merchant

--- inc/classes/class-client-account.php
+++ inc/classes/class-client-account.php
@@ -26,11 +26,18 @@ class Client_Account {
 	protected $merchant_api_key;
 
 	/**
-	 * Dialog API root
+	 * Connector endpoint root.
 	 *
 	 * @var string
 	 */
-	protected $web_root;
+	protected $connector_root;
+
+	/**
+	 * API endpoint root.
+	 *
+	 * @var string
+	 */
+	protected $api_root;
 
 	/**
 	 * Merchant ID.
@@ -47,7 +54,14 @@ class Client_Account {
 	protected $merchant_region;
 
 	/**
-	 * Store common values used in the plugin.
+	 * Algorithm used to sign the key.
+	 *
+	 * @var string
+	 */
+	protected $secret_algo = 'sha224';
+
+	/**
+	 * Store connector endpoint used in the plugin.
 	 *
 	 * @var array Common values used for api related info throughout the plugin.
 	 */
@@ -59,6 +73,22 @@ class Client_Account {
 		'US' => [
 			'sandbox' => 'https://connector.sandbox.uselaterpaytest.com',
 			'live'    => 'https://connector.uselaterpay.com',
+		],
+	];
+
+	/**
+	 * Store api endpoints used in the plugin.
+	 *
+	 * @var array Common values used for api related info throughout the plugin.
+	 */
+	public static $api_endpoints = [
+		'EU' => [
+			'sandbox' => 'https://api.sandbox.laterpaytest.net',
+			'live'    => 'https://api.laterpay.net/validatesignature',
+		],
+		'US' => [
+			'sandbox' => 'https://api.sandbox.uselaterpaytest.com',
+			'live'    => 'https://api.uselaterpay.com',
 		],
 	];
 
@@ -95,7 +125,7 @@ class Client_Account {
 	}
 
 	/**
-	 * Validate provided merchant credentials and make test purchase.
+	 * Validate provided merchant credentials and make test sure domain is valid.
 	 *
 	 * @return bool
 	 */
@@ -103,15 +133,19 @@ class Client_Account {
 		$global_options = Config::get_global_options();
 		$region         = $global_options['merchant_region'];
 
+		// Setup web roots for API Call.
+		$this->merchant_region = $region;
+
 		// Bail early, if no region set.
 		if ( empty( $region ) ) {
 			return false;
 		}
 
-		// Setup web root for API Call.
-		$this->merchant_region = $region;
-		$region_endpoints      = self::$connector_endpoints[ $region ];
-		$this->web_root        = $region_endpoints['sandbox']; // @todo make it live after testing.
+		$region_connector_endpoints = self::$connector_endpoints[ $region ];
+		$region_api_endpoints       = self::$api_endpoints[ $region ];
+
+		$this->connector_root = $region_connector_endpoints['sandbox']; // @todo make it live after testing.
+		$this->api_root       = $region_api_endpoints['sandbox']; // @todo make it live after testing.
 
 		// Setup merchant credentials.
 		$merchant_credentials = self::get_merchant_credentials();
@@ -123,7 +157,15 @@ class Client_Account {
 			$this->merchant_api_key = $merchant_credentials['merchant_key'];
 		}
 
-		return $this->test_sample_purchase();
+		// Check if account credentials are valid.
+		$are_credentials_valid = $this->test_merchant_credentials();
+
+		// If account credentials are valid, proceed to checking the merchant domain.
+		if ( true === $are_credentials_valid ) {
+			return $this->test_merchant_domain();
+		} else {
+			return false;
+		}
 	}
 
 	/**
@@ -131,11 +173,11 @@ class Client_Account {
 	 *
 	 * @return bool
 	 */
-	private function test_sample_purchase() {
+	private function test_merchant_domain() {
 		// Request to fetch endpoint, and add home url as origin.
-		$purchase_url = $this->build_test_purchase_url();
-		$home_url     = get_home_url();
-		$response     = wp_remote_get( $purchase_url, [
+		$fetch_url = $this->build_test_fetch_url();
+		$home_url  = get_home_url();
+		$response  = wp_remote_get( $fetch_url, [
 			// phpcs:ignore WordPressVIPMinimum.Performance.RemoteRequestTimeout.timeout_timeout -- Request done only once during initial setup.
 			'timeout' => 60,
 			'headers' => [
@@ -158,14 +200,57 @@ class Client_Account {
 	 *
 	 * @return string
 	 */
-	private function build_test_purchase_url() {
+	private function build_test_fetch_url() {
 		// Demo params to verify merchant domain.
-		$purchase_url_params = build_query( [
+		$fetch_url_params = build_query( [
 			'article_title' => rawurlencode( 'Revenue Generator Demo Page' ),
 			'article_url'   => rawurlencode( get_home_url() )
 		] );
 
-		return $this->get_api_fetch_url() . '?' . $purchase_url_params;
+		return $this->get_api_fetch_url() . '?' . $fetch_url_params;
+	}
+
+	/**
+	 * Handle account credentials validation.
+	 *
+	 * @return bool
+	 */
+	private function test_merchant_credentials() {
+		$validate_signature_url = $this->build_validate_signature_url();
+		$response               = wp_remote_get( $validate_signature_url );
+		$response_body          = wp_remote_retrieve_body( $response );
+		$response_status        = wp_remote_retrieve_response_code( $response );
+
+		// Check if the request was successful and account is valid.
+		if ( ! empty( $response_status ) && 200 === $response_status ) {
+			// Check if current config is valid or not.
+			$validation_data = json_decode( $response_body, true );
+			if ( ! empty( $validation_data['is_valid'] ) && true === $validation_data['is_valid'] ) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * Create validate signature url for testing account credentials.
+	 *
+	 * @return string
+	 */
+	private function build_validate_signature_url() {
+		$validation_url_params = build_query( [
+			'cp'   => $this->merchant_id,
+			'salt' => md5( microtime( true ) ),
+			'ts'   => time(),
+		] );
+		$encoded_params        = urlencode( $validation_url_params );
+		$message               = 'GET&' . urlencode( $this->get_validate_signature_url() ) . '&' . $encoded_params;
+		$hmac                  = hash_hmac( $this->secret_algo, $message, $this->merchant_api_key );
+
+		return $this->get_validate_signature_url() . '?' . $validation_url_params . '&hmac=' . $hmac;
 	}
 
 	/**
@@ -174,6 +259,15 @@ class Client_Account {
 	 * @return string
 	 */
 	private function get_api_fetch_url() {
-		return $this->web_root . '/api/v2/fetch';
+		return $this->connector_root . '/api/v2/fetch';
+	}
+
+	/**
+	 * Get dialog add URL.
+	 *
+	 * @return string
+	 */
+	private function get_validate_signature_url() {
+		return $this->api_root . '/validatesignature';
 	}
 }
