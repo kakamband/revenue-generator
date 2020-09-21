312f217f37e42b9cf5cb328d6a5952b8c347d3c6
Tom Stark <tstark@laterpay.net>
2020-09-21 11:45:25 +0200

    Move Paywall removal modal template to separate file to avoid code duplicates

---
+++ templates/backend/modal-remove-paywall.php
@@ -0,0 +1,22 @@
+<!-- Template for paywall removal confirmation. -->
+<script type="text/template" id="tmpl-rg-modal-remove-paywall">
+	<div class="rev-gen-modal" id="rg-modal-remove-paywall">
+		<div class="rev-gen-modal__inner">
+			<h4 class="rev-gen-modal__title">
+				<?php esc_html_e( 'Are you sure you want to remove the paywall?', 'revenue-generator' ); ?>
+			</h4>
+			<p class="rev-gen-preview-main-remove-paywall-message">
+				<?php esc_html_e( 'This content will be visible to all users.', 'revenue-generator' ); ?>
+			</p>
+			<div class="rev-gen-modal__buttons">
+				<button id="rg_js_modal_confirm" class="rev-gen__button">
+					<?php esc_html_e( 'Yes, remove Paywall', 'revenue-generator' ); ?>
+				</button>
+				<button id="rg_js_modal_cancel" class="rev-gen__button rev-gen__button--secondary">
+					<?php esc_html_e( 'No, keep Paywall', 'revenue-generator' ); ?>
+				</button>
+			</div>
+		</div>
+	</div>
+	<div class="rev-gen-modal-overlay"></div>
+</script>
--- templates/backend/paywall/post-preview.php
+++ templates/backend/paywall/post-preview.php
@@ -486,28 +486,7 @@ $paywall_hide_class      = ( 'publish' === get_post_status( $paywall_id ) ) ? 'h
 	</div>
 </script>
 
-<!-- Template for paywall removal confirmation. -->
-<script type="text/template" id="tmpl-rg-modal-remove-paywall">
-	<div class="rev-gen-modal" id="rg-modal-remove-paywall">
-		<div class="rev-gen-modal__inner">
-			<h4 class="rev-gen-modal__title">
-				<?php esc_html_e( 'Are you sure you want to remove the paywall?', 'revenue-generator' ); ?>
-			</h4>
-			<p class="rev-gen-preview-main-remove-paywall-message">
-				<?php esc_html_e( 'This content will be visible to all users.', 'revenue-generator' ); ?>
-			</p>
-			<div class="rev-gen-modal__buttons">
-				<button id="rg_js_modal_confirm" class="rev-gen__button">
-					<?php esc_html_e( 'Yes, remove Paywall', 'revenue-generator' ); ?>
-				</button>
-				<button id="rg_js_modal_cancel" class="rev-gen__button rev-gen__button--secondary">
-					<?php esc_html_e( 'No, keep Paywall', 'revenue-generator' ); ?>
-				</button>
-			</div>
-		</div>
-	</div>
-	<div class="rev-gen-modal-overlay"></div>
-</script>
+<?php include( REVENUE_GENERATOR_PLUGIN_DIR . '/templates/backend/modal-remove-paywall.php' ); ?>
 
 <!-- Template for action to add paywall. -->
 <script type="text/template" id="tmpl-revgen-add-paywall">
