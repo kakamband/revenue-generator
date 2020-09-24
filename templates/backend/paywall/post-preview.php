aa0202784fb92acc89f9906e595ffc6363e4c3f7
Tom Stark <tstark@laterpay.net>
2020-09-24 13:46:15 +0200

    Move account activation modal to separate file

--- templates/backend/paywall/post-preview.php
+++ templates/backend/paywall/post-preview.php
@@ -535,35 +535,7 @@ $paywall_hide_class      = ( 'publish' === get_post_status( $paywall_id ) ) ? 'h
 	</div>
 </script>
 
-<!-- Template for account activation modal. -->
-<script type="text/template" id="tmpl-rg-modal-account-activation">
-	<div class="rev-gen-modal" id="rg-modal-account-activation">
-		<div class="rev-gen-modal__inner">
-			<a href="#" class="rev-gen-modal__close" id="rg_js_modal_close">x</a>
-
-			<?php if ( true || false === $is_merchant_verified ) : ?>
-				<h4 class="rev-gen-modal__title">
-					<?php esc_html_e( 'You’re almost done!', 'revenue-generator' ); ?>
-				</h4>
-				<p class="rev-gen-modal__message">
-					<?php esc_html_e( 'To make sure you get your revenues, we need you to connect your Laterpay account.', 'revenue-generator' ); ?>
-				</p>
-				<div class="rev-gen-modal__buttons">
-					<button id="rg_js_modal_confirm" class="rev-gen__button">
-						<?php esc_html_e( 'Connect Account', 'revenue-generator' ); ?>
-					</button>
-					<button id="rg_js_modal_cancel" class="rev-gen__button rev-gen__button--secondary">
-						<?php esc_html_e( 'Signup', 'revenue-generator' ); ?>
-					</button>
-				</div>
-				<p>
-					<a href="https://support.laterpay.net/what-is-laterpay/" target="_blank" rel="noopener noreferrer"><?php esc_html_e( 'Learn more', 'revenue-generator' ); ?></a>
-				</p>
-			<?php endif; ?>
-		</div>
-	</div>
-	<div class="rev-gen-modal-overlay"></div>
-</script>
+<?php include( REVENUE_GENERATOR_PLUGIN_DIR . '/templates/backend/modal-account-activation.php' ); ?>
 
 <!-- Template for account activation modal. -->
 <script type="text/template" id="tmpl-rg-modal-connect-account">
