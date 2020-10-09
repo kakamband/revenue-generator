de063028b8f1ed114884d21c85c05c854533f773
Tom Stark <tstark@laterpay.net>
2020-10-09 12:40:07 +0200

    Compose shortcode out of default metas in preview.

--- templates/backend/contribution/preview.php
+++ templates/backend/contribution/preview.php
@@ -9,6 +9,24 @@ if ( ! defined( 'ABSPATH' ) ) {
 	// prevent direct access to this file.
 	exit;
 }
+
+use \LaterPay\Revenue_Generator\Inc\Post_Types\Contribution;
+
+$contribution_instance = Contribution::get_instance();
+$default_metas         = $contribution_instance->get_default_meta();
+$layout_type           = ( isset( $_GET['layout_type'] ) ) ? sanitize_text_field( $_GET['layout_type'] ) : 'box';
+$shortcode             = sprintf(
+	'[laterpay_contribution name="%s" thank_you="%s" type="%s" custom_amount="%s" all_amounts="%s" all_revenues="%s" selected_amount="1" dialog_header="%s" dialog_description="%s" layout_type="%s"]',
+	$default_metas['name'],
+	$default_metas['thank_you'],
+	$default_metas['type'],
+	$default_metas['custom_amount'],
+	join( ',', $default_metas['all_amounts'] ),
+	$default_metas['all_revenues'],
+	$default_metas['dialog_header'],
+	$default_metas['dialog_description'],
+	$layout_type
+);
 ?>
 <!DOCTYPE html>
 <html lang="en" class="rev-gen-preview">
@@ -21,7 +39,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 </head>
 <body>
 	<main>
-		<?php echo do_shortcode( '[laterpay_contribution id="66" layout_type="box"]' ); ?>
+		<?php echo do_shortcode( $shortcode ); ?>
 	</main>
 </body>
 </html>
