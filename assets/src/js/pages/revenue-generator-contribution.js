2b18db447d5acaa6ab954c215ffbc08c42afc0ea
Tom Stark <tstark@laterpay.net>
2020-11-04 18:39:37 +0100

    Add tourstep

--- assets/src/js/pages/revenue-generator-contribution.js
+++ assets/src/js/pages/revenue-generator-contribution.js
@@ -360,7 +360,8 @@ window.handleIframeLoad = ( iframe ) => {
 
 					props.buttons = buttonsProp;
 
-					const tourStep = self.tour.addStep( props )
+					const tourStep = self.tour
+						.addStep( props )
 						.on( 'before-hide', () => {
 							const optionClasses = tourStep.options.classes;
 							tourStep.options.classes = optionClasses.replace(
